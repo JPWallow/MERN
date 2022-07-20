ab51cf3583ddfbbdedd650259486916a50bdd183
Joshua Wallowitch <55968853+JPWallow@users.noreply.github.com>
2022-07-20 15:00:41 -0400

    Update mongoose.config.js

--- jokes-api-sub/server/config/mongoose.config.js
+++ jokes-api-sub/server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/jokes_db', {
+mongoose.connect('mongodb://127.0.0.1:27017/jokes_db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
