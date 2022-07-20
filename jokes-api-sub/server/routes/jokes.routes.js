346f49d4d616f0b44bc9516a3b448581cf45b68e
Joshua Wallowitch <55968853+JPWallow@users.noreply.github.com>
2022-07-20 15:01:19 -0400

    Update jokes.routes.js

--- jokes-api-sub/server/routes/jokes.routes.js
+++ jokes-api-sub/server/routes/jokes.routes.js
@@ -1,9 +1,9 @@
 const JokesController = require ("../controllers/jokes.controller.js");
 
 module.exports = (app) => {
-    app.get("api/jokes", JokesController.findAllJokes);
-    app.post("api/jokes", JokesController.createNewJoke);
-    app.get("api/jokes/:_id", JokesController.findOneJoke);
-    app.put("api/jokes/:_id", JokesController.updateJoke);
-    app.delete("api/jokes/:_id", JokesController.deleteJoke);
+    app.get("/api/jokes", JokesController.findAllJokes);
+    app.post("/api/jokes", JokesController.createNewJoke);
+    app.get("/api/jokes/:_id", JokesController.findOneJoke);
+    app.put("/api/jokes/:_id", JokesController.updateJoke);
+    app.delete("/api/jokes/:_id", JokesController.deleteJoke);
 }
